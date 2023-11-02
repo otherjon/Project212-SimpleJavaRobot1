579888dafe1a2ba1618171499443ba092cb989e7
Jon S <git@emufarm.org>
2023-11-01 22:50:41 -0700

    add DetectorSubsystem.java to represent IR proximity detector

---
+++ src/main/java/frc/robot/subsystems/DetectorSubsystem.java
@@ -0,0 +1,31 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.subsystems;
+
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import edu.wpi.first.wpilibj.DigitalInput;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+
+public class DetectorSubsystem extends SubsystemBase {
+  public DigitalInput sensor;
+  public boolean sensorValue;
+
+  /** Creates a new DetectorSubsystem. */
+  public DetectorSubsystem(int digitalIO_port) {
+    sensor = new DigitalInput(digitalIO_port);
+    sensorValue = sensor.get();
+    SmartDashboard.putBoolean("Sensor value", sensorValue);
+  }
+
+  public boolean something_is_near() {
+    return sensorValue;
+  }
+
+  @Override
+  public void periodic() {
+    sensorValue = sensor.get();
+    SmartDashboard.putBoolean("Sensor value", sensorValue);
+  }
+}
